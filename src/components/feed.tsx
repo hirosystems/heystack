ca221c12e8223c1528659e09a0421b08aaa0116e
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-06-03 08:19:42 -0500

    fix: textarea, word wrap

--- src/components/feed.tsx
+++ src/components/feed.tsx
@@ -69,6 +69,7 @@ const FeedItemComponent = ({
       initial={{ opacity: 0, y: 50, scale: 0.3 }}
       animate={{ opacity: 1, y: 0, scale: 1 }}
       exit={{ opacity: 0, scale: 0.5, transition: { duration: 0.2 } }}
+      wordBreak="break-word"
       {...rest}
     >
       <Flex alignItems="flex-end">
