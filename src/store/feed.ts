294b0da51cc47079e10663b91bf2802678698554
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-06-02 19:51:15 -0500

    feat: compose

--- src/store/feed.ts
+++ src/store/feed.ts
@@ -1,6 +1,11 @@
 import { atom } from 'jotai';
 import { feed } from '@common/feed';
+import { atomWithDefault } from 'jotai/utils';
 
 export const composeHeystackAom = atom('');
 
-export const feedAtom = atom(feed);
+export const feedAtom = atomWithDefault(async () => {
+  // TODO: replace with real fetching async logic
+  await new Promise(resolve => setTimeout(resolve, 2500));
+  return feed;
+});
