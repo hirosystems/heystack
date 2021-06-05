ae93970b935b3eeb1c281a052bb0746e4418f4bc
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-06-05 10:02:56 -0500

    fix: on every render we want to scroll

--- src/common/hooks/use-scroll-to-bottom.ts
+++ src/common/hooks/use-scroll-to-bottom.ts
@@ -4,6 +4,6 @@ export function useScrollToBottom(enabled: boolean) {
   const ref = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
     if (enabled) setTimeout(() => ref.current?.scrollIntoView({ behavior: 'smooth' }), 50);
-  }, [enabled]);
+  });
   return ref;
 }
